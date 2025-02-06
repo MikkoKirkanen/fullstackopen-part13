b1b39697949dd35f88c74562c0312f384166e025
Mikko Kirkanen <mikko.kirkanen@gmail.com>
2025-02-07 00:05:54 +0200

    Exercises 13.1.-13.3.

---
+++ commands.sql
@@ -0,0 +1,10 @@
+CREATE TABLE blogs (
+  id bigint GENERATED ALWAYS AS IDENTITY UNIQUE,
+  author text,
+  url text NOT NULL,
+  title text NOT NULL,
+  likes integer DEFAULT 0
+);
+
+INSERT INTO blogs (author, url, title) VALUES ('Mikko Kirkanen', 'https://mikkokirkanen.fi', 'Tervetuloa - Mikko Kirkanen');
+INSERT INTO blogs (author, url, title) VALUES ('Mikko Kirkanen', 'https://mikkokirkanen.fi/ohjelmointi-ja-projektit/muistipeli', 'Muistipeli - Ohjelmointi ja projektit - Mikko Kirkanen');
